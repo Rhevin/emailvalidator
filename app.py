74d4d1031ebfde40cafdce2ec562039595bcafab
Gugus <12741377+googoos@users.noreply.github.com>
2019-12-10 12:54:33 +0700

    20191210 1254

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ with open('test_email.csv') as email_list_csv:
         if is_valid == True:
             validated_email = [row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],str(is_valid)]
             write_output(validated_email)
-            print(validated_email)
+            # print(validated_email)
